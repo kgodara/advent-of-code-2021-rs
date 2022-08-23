51169fb50c2df43b1068e486e4ed2268989c7631
Karan Godara <karan@getquilt.app>
2022-08-23 04:16:03 -0400

    impl/perf: replace contains calls with split_whitespace

--- src/d2/p2.rs
+++ src/d2/p2.rs
@@ -1,24 +1,23 @@
 pub fn exec(src: String) {
 
     let mut aim_val: i32 = 0;
-
-    let mut temp_forward: u32;
-    let mut forward_val: u32 = 0;
-
+    let mut forward_val: i32 = 0;
     let mut depth_val: i32 = 0;
 
     for move_str in src.lines() {
-        if move_str.contains("forward") {
-            temp_forward = move_str.replace("forward ", "").parse::<u32>().unwrap();
-            forward_val += temp_forward;
-            depth_val += aim_val * ( temp_forward as i32 );
+        let mut move_args = move_str.split_whitespace();
+        let dir = move_args.next().unwrap();
+        let val = move_args.next().unwrap().parse::<i32>().unwrap();
 
-        } else if move_str.contains("up") {
-            aim_val -= move_str.replace("up ", "").parse::<i32>().unwrap();
-        } else if move_str.contains("down") {
-            aim_val += move_str.replace("down ", "").parse::<i32>().unwrap();
+        if dir == "forward" {
+            forward_val += val;
+            depth_val += aim_val * val;
+        } else if dir == "up" {
+            aim_val -= val;
+        } else if dir == "down" {
+            aim_val += val;
         }
     }
 
-    println!("result: {:?}", (forward_val as i32)*depth_val);
+    println!("result: {:?}", forward_val * depth_val);
 }
