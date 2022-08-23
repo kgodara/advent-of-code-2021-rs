3829331a22ddcc5b0c5b9233064eb7fa85afea49
Karan Godara <karan@getquilt.app>
2022-08-23 04:06:56 -0400

    impl/perf improvement: improved parsing, used iter methods (windows,map,sum) for sol

--- src/d1/p1.rs
+++ src/d1/p1.rs
@@ -1,26 +1,7 @@
-use std::process;
 
 pub fn exec(src: String) {
 
-    let mut input_data: Vec<u16> = Vec::new();
-    for num_str in src.split_whitespace() {
-        input_data.push(num_str.parse::<u16>().unwrap());
-    }
+    let input_data: Vec<u16> = src.split_whitespace().map(|num_str| num_str.parse::<u16>().unwrap()).collect();
 
-    if input_data.len() < 2 {
-        println!("0");
-        process::exit(1);
-    }
-
-    let mut prev: u16 = input_data[1];
-    let mut num_increase: u16 = 0;
-    
-    for num in input_data[1..].iter() {
-        if num > &prev {
-            num_increase += 1;
-        }
-        prev = *num;
-    }
-
-    println!("result: {:?}", num_increase);
+    println!("result: {}", input_data.windows(2).map(|pair| if pair[1] > pair[0] { 1 } else { 0 }).sum::<u16>());
 }
