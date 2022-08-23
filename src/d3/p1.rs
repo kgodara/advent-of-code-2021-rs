3267548d663897fb9ff55accd3a1f45407987718
Karan Godara <karan@getquilt.app>
2022-08-23 04:18:04 -0400

    impl/perf: replace min heap & hashmap & cmp impl with generalized sol with vec of freqs

--- src/d3/p1.rs
+++ src/d3/p1.rs
@@ -1,69 +1,30 @@
-use std::cmp::Ordering;
-use std::collections::BinaryHeap;
-
-use std::collections::HashMap;
-// Use a HashMap to store digit frequencies, could use a vec as well
-// Note: Handles input with different length binary numbers
-
-#[derive(Eq, Debug)]
-struct DigitFreq {
-    idx: u32,
-    freq: u32,
-}
-
-impl Ord for DigitFreq {
-    fn cmp(&self, other: &Self) -> Ordering {
-        other.idx.cmp(&self.idx)
-    }
-}
-
-impl PartialOrd for DigitFreq {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        other.idx.partial_cmp(&self.idx)
-    }
-}
-
-impl PartialEq for DigitFreq {
-    fn eq(&self, other: &Self) -> bool {
-        self.idx == other.idx
-    }
-}
 
 pub fn exec(src: String) {
 
-    // left-to-right indexed digit freqs
-    let mut digit_freqs: HashMap<u32, u32> = HashMap::new();
-    let mut num_nums: u32 = 0;
+    // 12 bit binary numbers
+    let mut digit_freqs: Vec<u32> = vec![0; 12];
+
+    let mut bin_num_count: u32 = 0;
 
     for bin_str in src.lines() {
-        for (digit_idx, digit) in bin_str.chars().enumerate() {
-            if digit == '1' {
-                *digit_freqs.entry(digit_idx as u32).or_insert(0) += 1;
+        for (digit_idx, digit)in bin_str.bytes().enumerate() {
+            if digit == b'1' {
+                digit_freqs[digit_idx] += 1;
             }
         }
-        num_nums += 1;
+        bin_num_count += 1;
     }
-
-    let mut heap: BinaryHeap<DigitFreq> = BinaryHeap::new();
-
-    let mut digit_idx: u32 = 0;
 
     let mut gamma_rate: u64 = 0;
     let mut epsilon_rate: u64 = 0;
 
-    // sort frequences on min-heap
-    while let Some(freq) = digit_freqs.get(&digit_idx) {
-        heap.push(DigitFreq { idx: digit_idx, freq: *freq });
-        digit_idx += 1;
-    }
-
-    while let Some(digit_freq) = heap.pop() {
+    for freq in digit_freqs {
 
         gamma_rate <<= 1;
         epsilon_rate <<= 1;
 
         // Is 1 the most common value in this place?
-        if digit_freq.freq >= (num_nums / 2) {
+        if freq >= (bin_num_count / 2) {
             gamma_rate += 1;
         } else {
             epsilon_rate += 1;
