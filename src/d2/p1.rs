436441944f3e0336fb5d3a4a25aea4277c3101ea
Karan Godara <karan@getquilt.app>
2022-08-23 04:11:01 -0400

    impl/perf: replace contains calls with split_whitespace

--- src/d2/p1.rs
+++ src/d2/p1.rs
@@ -1,16 +1,21 @@
 pub fn exec(src: String) {
-    let mut forward_val: u32 = 0;
+
+    let mut forward_val: i32 = 0;
     let mut depth_val: i32 = 0;
 
     for move_str in src.lines() {
-        if move_str.contains("forward") {
-            forward_val += move_str.replace("forward ", "").parse::<u32>().unwrap();
-        } else if move_str.contains("up") {
-            depth_val -= move_str.replace("up ", "").parse::<i32>().unwrap();
-        } else if move_str.contains("down") {
-            depth_val += move_str.replace("down ", "").parse::<i32>().unwrap();
+        let mut move_args = move_str.split_whitespace();
+        let dir = move_args.next().unwrap();
+        let val = move_args.next().unwrap().parse::<i32>().unwrap();
+
+        if dir == "forward" {
+            forward_val += val;
+        } else if dir == "up" {
+            depth_val -= val;
+        } else if dir == "down" {
+            depth_val += val;
         }
     }
 
-    println!("result: {:?}", (forward_val as i32)*depth_val);
+    println!("result: {:?}", forward_val * depth_val);
 }
