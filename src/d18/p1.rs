e95fc07fef04fd352ecd33a2aa7aab69fa415071
Karan Godara <karan@getquilt.app>
2022-10-05 21:13:55 -0400

    d18 REFACTOR >10x speedup; replace Rc<RefCell<>> with binary tree approach; merge & reduce trees to do addition; can further optimize by memoizing visual left->right traversal info

--- src/d18/p1.rs
+++ src/d18/p1.rs
@@ -1,249 +1,343 @@
-use std::fmt;
-
-use std::rc::Rc;
-use std::cell::RefCell;
-
-#[derive(Clone)]
-struct Element {
-    // can't have a literal as a parent
-    parent: Option<Rc<RefCell<Element>>>,
-    value: Option<Rc<RefCell<u8>>>,
-    children: Vec<Rc<RefCell<Element>>>,
+#[derive(Clone, Copy, Debug)]
+enum TreeElem {
+    Value(u8),
+    Parent,
 }
 
-impl fmt::Display for Element {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if let Some(literal_val) = &self.value {
-            write!(f, "{}", literal_val.borrow())?;
-        } else {
-            if self.children.len() != 2 {
-                panic!("Invalid Pair, must have at two children");
-            }
-            write!(f, "[")?;
+// Accepts: idx of element to search relative to
+// Returns: idx
+fn find_next_val(num_tree: &Vec<Option<TreeElem>>, ref_idx: u8, search_first_left: bool, next_val_stack: &mut [u8; 32]) -> Option<u8> {
 
-            write!(f, "{}", self.children[0].borrow())?;
-            
-            write!(f, ", ")?;
+    next_val_stack[0] = 0;
+    let mut stack_size: u8 = 1;
 
-            write!(f, "{}", self.children[1].borrow())?;
 
-            write!(f, "]")?;
-        }
-        Ok(())
-    }
-}
+    let mut cur_node_idx: u8;
+    let mut ref_node_seen: bool = false;
 
-// (element, depth)
-type ElementWithDepth = (Rc<RefCell<Element>>, u8);
+    let mut last_seen_val_idx: Option<u8> = None;
 
-fn exec_explode(dfs_stack: &mut Vec<ElementWithDepth>) -> bool {
-    // Literal
-    let mut left_explode_target_opt: Option<Rc<RefCell<Element>>> = None;
+    // Look for leftmost num >= 10
+    while stack_size != 0 {
+        cur_node_idx = next_val_stack[(stack_size-1) as usize];
+        stack_size -= 1;
 
-    let mut cur_elem: ElementWithDepth;
-    let mut cur_depth: u8;
-
-    let mut searching_right_literal: bool = false;
-    let mut right_literal_to_add: u8 = 0;
-
-    while !dfs_stack.is_empty() {
-        cur_elem = dfs_stack.pop().unwrap();
-        cur_depth = cur_elem.1;
-
-        if searching_right_literal {
-
-            if let Some(literal_val_ref) = &cur_elem.0.borrow().value {
-                *literal_val_ref.borrow_mut() += right_literal_to_add;
-                return true;
-            } else {
-                // push children in reversed order
-                dfs_stack.push((Rc::clone(&cur_elem.0.borrow().children[1]), cur_depth+1));
-                dfs_stack.push((Rc::clone(&cur_elem.0.borrow().children[0]), cur_depth+1));
-            }
-
-            continue;
+        if cur_node_idx == ref_idx && search_first_left {
+            return last_seen_val_idx;
         }
 
-        let left_explode_val: u8;
+        match num_tree[cur_node_idx as usize].unwrap() {
+            TreeElem::Parent => {
+                // push left child after, traverse left -> right
+                next_val_stack[stack_size as usize] = cur_node_idx * 2 + 2;
+                stack_size += 1;
 
-        // Determine operations to perform and set params
-        // Pair
-        if cur_elem.0.borrow().value.is_none() {
-
-            // When evaluating pair where left child is >= 10:
-            //     do explode since otherwise would have to try to add a pair
-            if cur_depth >= 4 {
-
-                ( left_explode_val, right_literal_to_add ) = ( *cur_elem.0.borrow().children[0].borrow().value.as_ref().unwrap().borrow(), *cur_elem.0.borrow().children[1].borrow().value.as_ref().unwrap().borrow());
-
-                if let Some(left_explode_target) = &left_explode_target_opt {
-                    if let Some(literal_ref) = &left_explode_target.borrow_mut().value {
-                        *literal_ref.borrow_mut() += left_explode_val;
-                    }
+                next_val_stack[stack_size as usize] = cur_node_idx * 2 + 1;
+                stack_size += 1;
+            },
+            TreeElem::Value(_val) => {
+                if ref_node_seen && !search_first_left {
+                    return Some(cur_node_idx)
                 }
+                last_seen_val_idx = Some(cur_node_idx);
+            },
+        }
 
-                let cur_parent = cur_elem.0.borrow().parent.clone();
+        ref_node_seen = cur_node_idx == ref_idx || ref_node_seen;
+        
+    }
+    None
+}
 
-                *cur_elem.0.borrow_mut() = Element {
-                    parent: cur_parent,
-                    value: Some(Rc::new(RefCell::new(0))),
-                    children: vec![],
-                };
-                searching_right_literal = true;
-            } else {
-                dfs_stack.push((Rc::clone(&cur_elem.0.borrow().children[1]), cur_depth+1));
-                dfs_stack.push((Rc::clone(&cur_elem.0.borrow().children[0]), cur_depth+1));
-            }
+fn exec_explode(num_tree: &mut Vec<Option<TreeElem>>, dfs_stack: &mut [u8; 32]) -> bool {
+
+    // Search for first eligible pair to explode:
+    // Depths and Indexes:
+    //     Depth-1, Index, 0-0
+    //     Depth-2, Index, 1-2
+    //     Depth-3, Index, 3-6
+    //     Depth-4, Index, 7-14
+    //     Depth-5, Index, 15-30
+    // The first TreeElem::Parent in ranges 15-31 is target to explode
+
+    /*
+    [ 1-2
+        [ 3-6
+            [ 7-14
+                [ 15-31
+                    [9,8],
+                    1
+                ],
+                2
+            ],
+            3
+        ],
+        4
+    ]
+    */
 
 
-        } else {
-            left_explode_target_opt = Some(Rc::clone(&cur_elem.0));
+
+    // Get idx of first explodable element
+    let mut to_explode_idx: u8 = 0;
+    let mut explode_left: u8 = 0;
+    let mut explode_right: u8 = 0;
+
+    for (idx, elem_opt) in num_tree.iter().enumerate().skip(15) {
+        if idx > 31 { break; }
+
+        if let Some(TreeElem::Parent) = elem_opt {
+
+            // Get Child values & Replace Pair with 0
+            explode_left = match num_tree[idx * 2 + 1].unwrap() {
+                TreeElem::Value(val) => {val},
+                TreeElem::Parent => {panic!("Two levels of invalid pairs found!")}
+            };
+
+            explode_right = match num_tree[idx * 2 + 2].unwrap() {
+                TreeElem::Value(val) => {val},
+                TreeElem::Parent => {panic!("Two levels of invalid pairs found!")}
+            };
+
+
+            elem_opt.unwrap();
+
+            to_explode_idx = idx as u8;
+            break;
         }
     }
+
+    if to_explode_idx == 0 { return false; }
+
+    // Replace Parent & children with 0
+
+    num_tree[to_explode_idx as usize] = Some(TreeElem::Value(0));
+    num_tree[(to_explode_idx * 2 + 1) as usize] = None;
+    num_tree[(to_explode_idx * 2 + 2) as usize] = None;
+    
+
+    // find idx of leftmost element, do addition if found
+    if let Some(first_left_elem) = find_next_val(&num_tree, to_explode_idx, true, dfs_stack) {
+
+        num_tree[first_left_elem as usize] = match num_tree[first_left_elem as usize].unwrap() {
+            TreeElem::Parent => {panic!("Can't increment value")},
+            TreeElem::Value(val) => {
+                Some(TreeElem::Value(explode_left+val))
+            }
+        };
+    }
+
+
+    // find idx of rightmost element, do addition if found
+    if let Some(first_right_elem) = find_next_val(&num_tree, to_explode_idx, false, dfs_stack) {
+
+        num_tree[first_right_elem as usize] = match num_tree[first_right_elem as usize].unwrap() {
+            TreeElem::Parent => {panic!("Can't increment value")},
+            TreeElem::Value(val) => {
+                Some(TreeElem::Value(explode_right+val))}
+        };
+    }
+
+    
+    true
+}
+
+fn exec_split(num_tree: &mut Vec<Option<TreeElem>>, dfs_stack: &mut [u8; 32]) -> bool {
+
+
+    // Traverse the tree left -> right, use a stack (need LIFO)
+    dfs_stack[0] = 0;
+    let mut stack_size: u8 = 1;
+
+    let mut cur_node_idx: u8;
+
+    // Look for leftmost num >= 10
+    while stack_size != 0 {
+        cur_node_idx = dfs_stack[(stack_size-1) as usize];
+        stack_size -= 1;
+
+        match num_tree[cur_node_idx as usize].unwrap() {
+            TreeElem::Parent => {
+                // push left child after, traverse left -> right
+                dfs_stack[stack_size as usize] = cur_node_idx * 2 + 2;
+                stack_size += 1;
+
+                dfs_stack[stack_size as usize] = cur_node_idx * 2 + 1;
+                stack_size += 1;
+            },
+            TreeElem::Value(val) => {
+                if val >= 10 {
+
+                    /*
+                        To split a regular number, replace it with a pair;
+                        the left element of the pair should be the regular number divided by two
+                        and rounded down, while the right element of the pair should be the
+                        regular number divided by two and rounded up.
+                    */
+                    num_tree[cur_node_idx as usize] = Some(TreeElem::Parent);
+                    num_tree[(cur_node_idx * 2 + 1) as usize] = Some(TreeElem::Value(val/2));
+                    num_tree[(cur_node_idx * 2 + 2) as usize] = Some(TreeElem::Value((val/2) + (val%2)));
+
+                    return true;
+                }
+            },
+        }
+    }
+
     false
 }
 
-fn exec_split(dfs_stack: &mut Vec<ElementWithDepth>) -> bool {
+fn get_magnitude(num_tree: &Vec<Option<TreeElem>>, start_idx: u8) -> u64 {
+
+    match num_tree[start_idx as usize].unwrap() {
+        TreeElem::Parent => {
+            (3 * get_magnitude(num_tree, start_idx*2+1)) +
+            (2 * get_magnitude(num_tree, start_idx*2+2))
+        },
+        TreeElem::Value(val) => {val as u64},
+    }
+}
 
 
-    let mut cur_elem: ElementWithDepth;
-    let mut cur_depth: u8;
+fn _print_tree_vals(tree: &Vec<Option<TreeElem>>) {
+    println!("( ");
 
+    let mut dfs_stack: Vec<usize> = vec![0];
+
+    let mut cur_node_idx: usize;
     while !dfs_stack.is_empty() {
-        cur_elem = dfs_stack.pop().unwrap();
-        cur_depth = cur_elem.1;
+        cur_node_idx = dfs_stack.pop().unwrap();
 
-        if cur_elem.0.borrow().value.is_some() {
-
-            let literal_val_ref = &cur_elem.0.borrow().value.clone().unwrap();
-            if *literal_val_ref.borrow() > 9 {
-
-                let val_to_split: u8 = *literal_val_ref.borrow();
-
-                let left_literal: u8 = val_to_split / 2;
-                let right_literal: u8 = (val_to_split / 2) + (val_to_split % 2);
-
-                let cur_parent = cur_elem.0.borrow().parent.clone();
-
-                *cur_elem.0.borrow_mut() = Element {
-                    parent: cur_parent,
-                    value: None,
-                    children: vec![
-                        Rc::new(RefCell::new(Element { parent: Some(Rc::clone(&cur_elem.0)), value: Some(Rc::new(RefCell::new(left_literal))), children: vec![] })),
-                        Rc::new(RefCell::new(Element { parent: Some(Rc::clone(&cur_elem.0)), value: Some(Rc::new(RefCell::new(right_literal))), children: vec![] })),                                
-                    ]
-                };
-
-                // No need to push to stack, change made, can break
-                return true;
+        match tree[cur_node_idx].unwrap() {
+            TreeElem::Parent => {
+                // push left child after, traverse left -> right
+                dfs_stack.push(cur_node_idx * 2 + 2);
+                dfs_stack.push(cur_node_idx * 2 + 1);
+            },
+            TreeElem::Value(val) => {
+                if cur_node_idx >= 1 && cur_node_idx <= 2 {
+                    print!("  ");
+                } else if cur_node_idx >= 3 && cur_node_idx <= 6 {
+                    print!("    ");
+                } else if cur_node_idx >= 7 && cur_node_idx <= 14 {
+                    print!("      ");
+                } else if cur_node_idx >= 15 && cur_node_idx <= 30 {
+                    print!("        ");
+                } else if cur_node_idx >= 31 && cur_node_idx <= 62 {
+                    print!("          ");
+                }
+                println!("{}", val);
             }
-        } else {
-            dfs_stack.push((Rc::clone(&cur_elem.0.borrow().children[1]), cur_depth+1));
-            dfs_stack.push((Rc::clone(&cur_elem.0.borrow().children[0]), cur_depth+1));
         }
     }
-    false
+    println!(")");
 }
 
-fn get_magnitude(elem: Rc<RefCell<Element>>) -> u64 {
-    if elem.borrow().value.is_some() {
-        *elem.borrow().value.as_ref().unwrap().borrow() as u64
-    } else {
-        (3*get_magnitude(Rc::clone(&elem.borrow().children[0]))) + (2*get_magnitude(Rc::clone(&elem.borrow().children[1])))
-    }
-}
+pub fn exec(src: &str, print: bool) {
 
-pub fn exec(src: String) {
+    let mut input_nums: Vec<Vec<Option<TreeElem>>> = vec![];
 
-    let mut input_data: Vec<Rc<RefCell<Element>>> = vec![];
+    // No single root node
+    let mut cur_idx = 0;
 
-    // parse input numbers
+
+    let mut cur_num: Vec<Option<TreeElem>>;
+
     for line in src.lines() {
+        // for easy indexing, constant is just an estimate
+        // max depth is 4, thus including leaf & internal nodes: (2^4+2^3+2^2+2^1+2^0) --> 31 max nodes
+        cur_num = vec![None; 31];
 
-        let mut cur_pair: Rc<RefCell<Element>> = Rc::new(RefCell::new(Element { parent: None, value: None, children: vec![] }));
+        for ch in line.chars() {
 
-        // skip the first opening bracket, already assumed
-        for ch in line.chars().skip(1) {
             if ch.is_digit(10) {
-                cur_pair.borrow_mut().children.push(Rc::new(RefCell::new(
-                    Element {
-                        parent: Some(Rc::clone(&cur_pair)),
-                        value: Some(Rc::new(RefCell::new(ch.to_digit(10).unwrap() as u8))),
-                        children: vec![],
-                    }
-                )));
+                cur_num[cur_idx] = Some(TreeElem::Value(ch.to_digit(10).unwrap() as u8));
             }
             else {
                 match ch {
                     '[' => {
-
-                        let new_child_pair = Rc::new(RefCell::new(Element {
-                            parent: Some(Rc::clone(&cur_pair)),
-                            value: None,
-                            children: vec![],
-                        }));
-
-                        cur_pair.borrow_mut().children.push(Rc::clone(&new_child_pair));
-                        cur_pair = Rc::clone(&new_child_pair);
+                        // descending a level down tree: *2 + 1
+                        cur_num[cur_idx] = Some(TreeElem::Parent);
+                        cur_idx = cur_idx * 2 + 1
                     },
                     ',' => {
+                        // sibling: + 1
+                        cur_idx += 1;
                     },
-                    // move back up to parent
                     ']' => {
-                        #[allow(clippy::needless_late_init)]
-                        let temp;
-                        // assignment on matched variable causes borrow-checker confusion
-                        match &cur_pair.borrow().parent {
-                            Some(parent_element) => { temp = Rc::clone(parent_element); },
-                            None => { break; },
-                        }
-
-                        cur_pair = temp;
+                        // move back up to parent: (...-1) / 2
+                        cur_idx = (cur_idx-1) / 2;
                     },
                     _ => { panic!("invalid char: [{}]", ch) }
                 }
             }
         }
 
-        input_data.push(cur_pair);
+        input_nums.push(cur_num);
     }
 
+    // will always have reduced num
+    let mut result: Vec<Option<TreeElem>>;
 
+    // can have up to depth 5, while numbers are being exploded/split
+    let mut to_merge: Vec<Option<TreeElem>> = vec![None; 63];
 
     // Compute results for each line
     // merge result with next snail num
-    let mut result: Rc<RefCell<Element>> = Rc::clone(&input_data[0]);
 
-    for snail_num in input_data.iter().skip(1) {
+    let mut generic_stack: [u8; 32] = [0; 32];
 
-        result = Rc::new(RefCell::new(Element {
-            parent: None,
-            value: None,
-            children: vec![ Rc::clone(&result), Rc::clone(snail_num) ]
-        }));
+    result = input_nums[0].clone();
+
+    for snail_num in input_nums.iter().skip(1) {
+        // merge binary trees [result, snail_num]
+
+        // populate to_merge:
+        //     result: left-half of each level 
+        //     snail_num: right-half of each level
+        //     everything is moved one level down due to merging
+
+        to_merge[0] = Some(TreeElem::Parent);
+
+        // root node (depth=0) already handled
+        for depth in 1..6 {
+            // let depth_len: usize = 2usize.pow(depth);
+
+            // fill each level
+            // Note: both of these are inclusive
+            // merge depth 2 (3-6) pulls from src depth 1 [1-2, 1-2]
+            let src_depth_start = 2usize.pow(depth-1)-1;
+            let src_depth_end = src_depth_start * 2;
+
+            let merge_depth_half_len = 2usize.pow(depth)/2;
+            let merge_depth_first_half = 2usize.pow(depth)-1;
+            let merge_depth_second_half = merge_depth_first_half + merge_depth_half_len;
+
+            to_merge[merge_depth_first_half..merge_depth_first_half+merge_depth_half_len].copy_from_slice(
+                &result[src_depth_start..(src_depth_end+1)]
+            );
+
+            to_merge[merge_depth_second_half..merge_depth_second_half+merge_depth_half_len].copy_from_slice(
+                &snail_num[src_depth_start..(src_depth_end+1)]
+            );
+        }
+
 
         let mut changed: bool = true;
-        // While iteration yields changes:
-        //     DFS iterate through number
+
         while changed {
-
-            // Note: result will always have 2 children, both of which must be pairs
-            // push in reversed order in order to traverse left --> right
-            let mut dfs_stack: Vec<ElementWithDepth> = vec![(Rc::clone(&result.borrow().children[1]), 1), (Rc::clone(&result.borrow().children[0]), 1)];
-
-            // set result's children's parents
-            let result_parent = Rc::clone(&result);
-            result.borrow_mut().children[0].borrow_mut().parent = Some(result_parent.clone());
-            result.borrow_mut().children[1].borrow_mut().parent = Some(result_parent);
-
-            changed = exec_explode(&mut dfs_stack);
+            changed = exec_explode(&mut to_merge, &mut generic_stack);
             if !changed {
-                dfs_stack = vec![(Rc::clone(&result.borrow().children[1]), 1), (Rc::clone(&result.borrow().children[0]), 1)];
-                changed = exec_split(&mut dfs_stack);
+                changed = exec_split(&mut to_merge, &mut generic_stack);
             }
+        }
+
+        // move now valid to_merge data into 'result' & reset to_merge
+        result[0..31].copy_from_slice(&to_merge[0..31]);
+        for val in to_merge.iter_mut() {
+            *val = None;
         }
     }
 
-    println!("result: {}", get_magnitude(Rc::clone(&result)));
-
+    if print { println!("result: {}", get_magnitude(&result, 0)) }
 }
