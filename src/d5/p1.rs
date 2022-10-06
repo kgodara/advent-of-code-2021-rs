7cb57e46f9455686829e3a4f66e7f025cd671740
Karan Godara <karan@getquilt.app>
2022-10-05 20:52:08 -0400

    d5 perf improvement; smaller datatypes, arrays, parse improvement, simplified logic

--- src/d5/p1.rs
+++ src/d5/p1.rs
@@ -5,55 +5,62 @@ struct Line {
     pub is_row: bool,
 
     // idx for range which doesn't change
-    pub primary_idx: usize,
+    pub primary_idx: u16,
 
-    pub bound_one: usize,
-    pub bound_two: usize,
+    pub bound_one: u16,
+    pub bound_two: u16,
+
+    // pub bound_range: Range<u16>,
 }
 
+const MAX_X: usize = 1000;
+const MAX_Y: usize = 1000;
 
 
 
-pub fn exec(src: String) {
+pub fn exec(src: &str, print: bool) {
 
     let mut lines: Vec<Line> = Vec::new();
     let mut is_row: bool;
 
-    let mut max_x: usize = 0;
-    let mut max_y: usize = 0;
+    let mut max_x: u16 = 0;
+    let mut max_y: u16 = 0;
     
     for line in src.lines() {
-       let pairs: Vec<&str> = line.split(" -> ").collect();
-
-       let start_coords: Vec<usize> = pairs[0].split(',')
-            .map(|num_str| num_str.parse().unwrap())
+        
+        let all_coords: Vec<u16> = line.split(" -> ")
+            .map(|coord_pair| {
+                coord_pair.split(',')
+            })
+            .flatten()
+            .map(|coord_str| {
+                coord_str.parse().unwrap()
+            })
             .collect();
 
-        let end_coords: Vec<usize> = pairs[1].split(',')
-            .map(|num_str| num_str.parse().unwrap())
-            .collect();
-
-        is_row = if start_coords[1] == end_coords[1] {
+        // start_coords[1] == end_coords[1]
+        is_row = if all_coords[1] == all_coords[3] {
             true
-        } else if start_coords[0] == end_coords[0] {
+        }
+        // start_coords[0] == end_coords[0]
+        else if all_coords[0] == all_coords[2] {
             false
         } else {
             continue;
         };
 
-        max_x = max(max_x, max(start_coords[0], end_coords[0]));
-        max_y = max(max_y, max(start_coords[1], end_coords[1]));
-        
+        max_x = max(max_x, max(all_coords[0], all_coords[2]));
+        max_y = max(max_y, max(all_coords[1], all_coords[3]));
+
         lines.push(Line { is_row,
-            primary_idx: if is_row { start_coords[1] } else { start_coords[0] },
-            bound_one: if is_row { start_coords[0] } else { start_coords[1] },
-            bound_two: if is_row { end_coords[0] } else { end_coords[1] }
+            primary_idx: if is_row { all_coords[1] } else { all_coords[0] },
+            bound_one: if is_row { all_coords[0] } else { all_coords[1] },
+            bound_two: if is_row { all_coords[2] } else { all_coords[3] }
         });
     }
 
-
     // +1 since upper_bounds are inclusive
-    let mut field: Vec<Vec<usize>> = vec![vec![0; max_x+1]; max_y+1];
+    let mut field: [[u16; MAX_X]; MAX_Y] = [[0; MAX_X]; MAX_Y];
 
     for line in lines.iter() {
         // upper bound is inclusive
@@ -61,9 +68,9 @@ pub fn exec(src: String) {
         if line.bound_one < line.bound_two { line.bound_one..(line.bound_two+1) } else { line.bound_two..(line.bound_one+1) } {
 
             if line.is_row {
-                field[line.primary_idx][cell_idx] += 1;
+                field[line.primary_idx as usize][cell_idx as usize] += 1;
             } else {
-                field[cell_idx][line.primary_idx] += 1;
+                field[cell_idx as usize][line.primary_idx as usize] += 1;
             }
         }
     }
@@ -75,7 +82,5 @@ pub fn exec(src: String) {
         }
     }
 
-    println!("result: {:?}", total);
-
-
+    if print { println!("result: {:?}", total) }
 }
--- src/d5/p2.rs
+++ src/d5/p2.rs
@@ -13,68 +13,74 @@ struct Line {
     // is this a (row|col) line or a diagonal
     pub line_type: LineType,
 
-    pub x_bound_one: usize,
-    pub y_bound_one: usize,
+    pub x_bound_one: i32,
+    pub y_bound_one: i32,
 
-    pub x_bound_two: usize,
-    pub y_bound_two: usize,
+    pub x_bound_two: i32,
+    pub y_bound_two: i32,
 }
 
+const MAX_X: usize = 1000;
+const MAX_Y: usize = 1000;
 
-pub fn exec(src: String) {
+
+pub fn exec(src: &str, print: bool) {
 
     let mut lines: Vec<Line> = Vec::new();
 
     let mut line_type: LineType;
 
-    let mut max_x: usize = 0;
-    let mut max_y: usize = 0;
+    let mut max_x: u16 = 0;
+    let mut max_y: u16 = 0;
 
     for line in src.lines() {
-       let pairs: Vec<&str> = line.split(" -> ").collect();
-
-       let start_coords: Vec<usize> = pairs[0].split(',')
-            .map(|num_str| num_str.parse().unwrap())
+        let all_coords: Vec<u16> = line.split(" -> ")
+            .map(|coord_pair| {
+                coord_pair.split(',')
+            })
+            .flatten()
+            .map(|coord_str| {
+                coord_str.parse().unwrap()
+            })
             .collect();
 
-        let end_coords: Vec<usize> = pairs[1].split(',')
-            .map(|num_str| num_str.parse().unwrap())
-            .collect();
-
-        max_x = max(max_x, max(start_coords[0], end_coords[0]));
-        max_y = max(max_y, max(start_coords[1], end_coords[1]));
-    
-
-        line_type = if start_coords[1] == end_coords[1] {
+        // start_coords[1] == end_coords[1]
+        line_type = if all_coords[1] == all_coords[3] {
             LineType::Row
-        } else if start_coords[0] == end_coords[0] {
+        }
+        // start_coords[0] == end_coords[0]
+        else if all_coords[0] == all_coords[2] {
             LineType::Col
         } else {
             LineType::Diag
         };
 
-        lines.push(Line { line_type,
-            x_bound_one: start_coords[0],
-            y_bound_one: start_coords[1],
+        max_x = max(max_x, max(all_coords[0], all_coords[2]));
+        max_y = max(max_y, max(all_coords[1], all_coords[3]));
+    
 
-            x_bound_two: end_coords[0],
-            y_bound_two: end_coords[1],
+        lines.push(Line { line_type,
+            x_bound_one: all_coords[0] as i32,
+            y_bound_one: all_coords[1] as i32,
+
+            x_bound_two: all_coords[2] as i32,
+            y_bound_two: all_coords[3] as i32,
         });
     }
 
     // +1 since upper_bounds are inclusive
-    let mut field: Vec<Vec<usize>> = vec![vec![0; max_x+1]; max_y+1];
+    let mut field: [[u16; MAX_X]; MAX_Y] = [[0; MAX_X]; MAX_Y];
 
-    let mut cell_x_idx: usize;
-    let mut cell_y_idx: usize = 0;
+    let mut cell_x_idx: i32;
+    let mut cell_y_idx: i32 = 0;
 
     let mut final_x;
     let mut final_y;
 
     for line in lines.iter() {
 
-        let is_x_bound_incr: bool = line.x_bound_one < line.x_bound_two;
-        let is_y_bound_incr: bool = line.y_bound_one < line.y_bound_two;
+        let x_bound_incr: i32 = if line.x_bound_one < line.x_bound_two { 1 } else { -1 };
+        let y_bound_incr: i32 = if line.y_bound_one < line.y_bound_two { 1 } else { -1 };
 
         cell_x_idx = 0;
 
@@ -91,38 +97,34 @@ pub fn exec(src: String) {
         // Iter over abs( difference )
         //     decide whether to incr or decr relative to (x|y)_bound_one based on if starting at larger|smaller
         // 
-        while (cell_x_idx as i32) <= ((line.x_bound_one as i32) - (line.x_bound_two as i32)).abs() {
+
+
+        final_x = line.x_bound_one;
+        final_y = line.y_bound_one;
+
+        while cell_x_idx <= (line.x_bound_one - line.x_bound_two).abs() {
 
 
             // if non-diagonal, need to set cell_y_idx here in case of horizontal line
             // since inner loop will only be executing once per outer loop iter
             if line.line_type != LineType::Diag {
                 cell_y_idx = 0;
+                final_y = line.y_bound_one;
             }
 
-            while (cell_y_idx as i32) <= ((line.y_bound_one as i32) - (line.y_bound_two as i32)).abs() {
+            while cell_y_idx <= (line.y_bound_one - line.y_bound_two).abs() {
 
-
-                if is_x_bound_incr {
-                    final_x = line.x_bound_one + cell_x_idx;
-                } else {
-                    final_x = line.x_bound_one - cell_x_idx;
-                }
-
-                if is_y_bound_incr {
-                    final_y = line.y_bound_one + cell_y_idx;
-                } else {
-                    final_y = line.y_bound_one - cell_y_idx;
-                }
-
-                field[final_y][final_x] += 1;
+                field[final_y as usize][final_x as usize] += 1;
 
                 cell_y_idx += 1;
+                final_y += y_bound_incr;
+
                 if line.line_type == LineType::Diag {
                     break;
                 }
             }
             cell_x_idx += 1;
+            final_x += x_bound_incr;
         }
     }
 
@@ -133,6 +135,5 @@ pub fn exec(src: String) {
         }
     }
 
-    println!("result: {:?}", total);
-
+    if print { println!("result: {:?}", total) }
 }
